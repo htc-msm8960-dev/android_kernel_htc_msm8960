b9359ddecd16946f61d75f3abfbda16577b70c40
Al Viro <viro@zeniv.linux.org.uk>
2012-06-10 18:05:36 -0400

    don't pass nameidata to ->create()
    boolean "does it have to be exclusive?" flag is passed instead;
    Local filesystem should just ignore it - the object is guaranteed
    not to be there yet.
    
    Change-Id: I25efea9892458f6f64070c62bd1adb5194dcd8c1
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/minix/namei.c
+++ fs/minix/namei.c
@@ -67,7 +67,7 @@ static int minix_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
 }
 
 static int minix_create(struct inode *dir, struct dentry *dentry, umode_t mode,
-		struct nameidata *nd)
+		bool excl)
 {
 	return minix_mknod(dir, dentry, mode, 0);
 }
