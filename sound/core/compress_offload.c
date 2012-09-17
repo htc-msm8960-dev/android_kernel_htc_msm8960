64afb96443e8f5a57993326a199ae4b535a12b6b
Vinod Koul <vinod.koul@linux.intel.com>
2012-09-17 11:51:25 +0530

    ALSA: compress - move the buffer check
    Commit ALSA: compress_core: integer overflow in snd_compr_allocate_buffer()
    added a new error check for input params.
    this add new routine for input checks and moves buffer overflow check to this
    new routine. This allows the error value to be propogated to user space
    
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -412,10 +412,6 @@ static int snd_compr_allocate_buffer(struct snd_compr_stream *stream,
 	unsigned int buffer_size;
 	void *buffer;
 
-	if (params->buffer.fragment_size == 0 ||
-	    params->buffer.fragments > SIZE_MAX / params->buffer.fragment_size)
-		return -EINVAL;
-
 	buffer_size = params->buffer.fragment_size * params->buffer.fragments;
 	if (stream->ops->copy) {
 		buffer = NULL;
@@ -438,7 +434,7 @@ static int snd_compress_check_input(struct snd_compr_params *params)
 {
 	/* first let's check the buffer parameter's */
 	if (params->buffer.fragment_size == 0 ||
-	    params->buffer.fragments > INT_MAX / params->buffer.fragment_size)
+			params->buffer.fragments > SIZE_MAX / params->buffer.fragment_size)
 		return -EINVAL;
 
 	return 0;
