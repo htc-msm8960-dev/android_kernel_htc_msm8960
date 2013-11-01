7c92dd06e99cf0295a88da4e61d1832fc3a40a34
Mayank Rana <mrana@codeaurora.org>
2013-11-01 15:38:39 +0530

    USB: gadget: Add support to enable streaming mode with required function
    Supported different usb functions' has different throughput requirement.
    USB controller supports streaming mode feature which enables double
    buffering scheme on both IN and OUT endpoint, and reduces number of
    NAKs for IN transfer. Hence noticeble throughput is increased with it.
    Currently streaming mode is disable with USB controller. As supported
    usb functions'like mtp, rndis are required to have higher throughput.
    Add support which allows to enable streaming mode with usb controller
    in device mode when particular function based composition is being enabled.
    
    CRs-Fixed: 550553
    Change-Id: Ibf6f3dc3f3b821647ba9beb15ed735a85c8ed586
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: D. Andrei Măceș <Andrei.Maces@alumni.nd.edu>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2257,7 +2257,20 @@ android_unbind_enabled_functions(struct android_dev *dev,
 			f_holder->f->unbind_config(f_holder->f, c);
 	}
 }
+static inline void check_streaming_func(struct usb_gadget *gadget,
+		struct android_usb_platform_data *pdata,
+		char *name)
+{
+	int i;
 
+	for (i = 0; i < pdata->streaming_func_count; i++) {
+		if (!strcmp(name, pdata->streaming_func[i])) {
+			pr_debug("set streaming_enabled to true\n");
+			gadget->streaming_enabled = true;
+			break;
+		}
+	}
+}
 static int android_enable_function(struct android_dev *dev,
 				   struct android_configuration *conf,
 				   char *name)
@@ -2265,6 +2278,9 @@ static int android_enable_function(struct android_dev *dev,
 	struct android_usb_function **functions = dev->functions;
 	struct android_usb_function *f;
 	struct android_usb_function_holder *f_holder;
+	struct android_usb_platform_data *pdata = dev->pdata;
+	struct usb_gadget *gadget = dev->cdev->gadget;
+
 	while ((f = *functions++)) {
 		if (!strcmp(name, f->name)) {
 			if (f->android_dev && f->android_dev != dev)
@@ -2282,6 +2298,13 @@ static int android_enable_function(struct android_dev *dev,
 				f_holder->f = f;
 				list_add_tail(&f_holder->enabled_list,
 					      &conf->enabled_functions);
+				pr_debug("func:%s is enabled.\n", f->name);
+				/*
+				 * compare enable function with streaming func
+				 * list and based on the same request streaming.
+				 */
+				check_streaming_func(gadget, pdata, f->name);
+
 				return 0;
 			}
 		}
@@ -2710,6 +2733,10 @@ static void android_unbind_config(struct usb_configuration *c)
 {
 	struct android_dev *dev = cdev_to_android_dev(c->cdev);
 
+	if (c->cdev->gadget->streaming_enabled) {
+		c->cdev->gadget->streaming_enabled = false;
+		pr_debug("setting streaming_enabled to false.\n");
+	}
 	android_unbind_enabled_functions(dev, c);
 }
 
