c11424f6931b259223646146e230c5583f673aa0
Miklos Szeredi <mszeredi@suse.cz>
2014-07-23 15:15:30 +0200

    BACKPORT: fs: call rename2 if exists
    Christoph Hellwig suggests:
    
    1) make vfs_rename call ->rename2 if it exists instead of ->rename
    2) switch all filesystems that you're adding NOREPLACE support for to
       use ->rename2
    3) see how many ->rename instances we'll have left after a few
       iterations of 2.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    @acroreiser: backport to 3.10, fuse changes omitted

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2667,7 +2667,6 @@ const struct inode_operations ext4_dir_inode_operations = {
 	.rmdir		= ext4_rmdir,
 	.mknod		= ext4_mknod,
 	.tmpfile	= ext4_tmpfile,
-	.rename		= ext4_rename,
 	.rename2	= ext4_rename2,
 	.setattr	= ext4_setattr,
 #ifdef CONFIG_EXT4_FS_XATTR
