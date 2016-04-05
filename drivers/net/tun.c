242758b05d547e0613f7a2fd2cfdc0a2a2a756b3
Hannes Frederic Sowa <hannes@stressinduktion.org>
2016-04-05 17:10:16 +0200

    tun: use socket locks for sk_{attach,detatch}_filter
    This reverts commit 5a5abb1fa3b05dd ("tun, bpf: fix suspicious RCU usage
    in tun_{attach, detach}_filter") and replaces it to use lock_sock around
    sk_{attach,detach}_filter. The checks inside filter.c are updated with
    lockdep_sock_is_held to check for proper socket locks.
    
    It keeps the code cleaner by ensuring that only one lock governs the
    socket filter instead of two independent locks.
    
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    Change-Id: I0d8a3986669a37668c39367a94f10a87a18509dc

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1442,8 +1442,9 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 		if (copy_from_user(&fprog, argp, sizeof(fprog)))
 			break;
 
-		ret = __sk_attach_filter(&fprog, tun->socket.sk,
-				lockdep_rtnl_is_held());
+		lock_sock(tun->socket.sk);
+		ret = sk_attach_filter(&fprog, tun->socket.sk);
+		release_sock(tun->socket.sk);
 		break;
 
 	case TUNDETACHFILTER:
@@ -1451,7 +1452,9 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 		ret = -EINVAL;
 		if ((tun->flags & TUN_TYPE_MASK) != TUN_TAP_DEV)
 			break;
-		ret = __sk_detach_filter(tun->socket.sk, lockdep_rtnl_is_held());
+                lock_sock(tun->socket.sk);
+		ret = sk_detach_filter(tun->socket.sk);
+                release_sock(tun->socket.sk);
 		break;
 
 	default:
