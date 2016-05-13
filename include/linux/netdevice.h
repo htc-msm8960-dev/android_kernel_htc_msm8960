e58b4f38acc8ad8f1f0e43686028c82763e7f3e8
Daniel Borkmann <daniel@iogearbox.net>
2016-05-13 19:08:27 +0200

    bpf: move bpf_jit_enable declaration
    Move the bpf_jit_enable declaration to the filter.h file where
    most other core code is declared, also since we're going to add
    a second knob there.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2586,7 +2586,6 @@ extern void netdev_stats_to_stats64(struct rtnl_link_stats64 *stats64,
 extern int		netdev_max_backlog;
 extern int		netdev_tstamp_prequeue;
 extern int		weight_p;
-extern int		bpf_jit_enable;
 extern int		netdev_set_master(struct net_device *dev, struct net_device *master);
 extern int netdev_set_bond_master(struct net_device *dev,
 				  struct net_device *master);
