7c7ff493356bee5d50cd6c85b0095a83cb684b72
TARKZiM <tom8476oo@gmail.com>
2019-12-08 00:50:49 +0800

    kernel: power: Remove unneeded touch attr

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -642,8 +642,6 @@ static struct attribute *g[] = {
 #ifdef CONFIG_PM_SLEEP
 	&pm_async_attr.attr,
 	&wakeup_count_attr.attr,
-	&touch_event_attr.attr,
-	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_AUTOSLEEP
 	&autosleep_attr.attr,
 #endif
