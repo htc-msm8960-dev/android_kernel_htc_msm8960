32f4046567177ab00abb6258097fc3366c5d6205
Yaroslav Zviezda <acroreiser@gmail.com>
2024-06-25 21:40:52 +0300

    Revert "security: selinux: Add Per-File-Encryption hooks"
    This reverts commit 16f60cc25515e661c905f4fc536317e8053d3ae5.

--- include/linux/security.h
+++ include/linux/security.h
@@ -26,7 +26,6 @@
 #include <linux/capability.h>
 #include <linux/slab.h>
 #include <linux/err.h>
-#include <linux/bio.h>
 #include <linux/bpf.h>
 
 struct linux_binprm;
@@ -1489,8 +1488,6 @@ struct security_operations {
 				    void **value, size_t *len);
 	int (*inode_create) (struct inode *dir,
 			     struct dentry *dentry, umode_t mode);
-	int (*inode_post_create) (struct inode *dir,
-				  struct dentry *dentry, umode_t mode);
 	int (*inode_link) (struct dentry *old_dentry,
 			   struct inode *dir, struct dentry *new_dentry);
 	int (*inode_unlink) (struct inode *dir, struct dentry *dentry);
@@ -1541,8 +1538,6 @@ struct security_operations {
 				    struct fown_struct *fown, int sig);
 	int (*file_receive) (struct file *file);
 	int (*dentry_open) (struct file *file, const struct cred *cred);
-	int (*file_close) (struct file *file);
-	bool (*allow_merge_bio)(struct bio *bio1, struct bio *bio2);
 
 	int (*task_create) (unsigned long clone_flags);
 	void (*task_free) (struct task_struct *task);
@@ -1782,9 +1777,6 @@ int security_old_inode_init_security(struct inode *inode, struct inode *dir,
 				     const struct qstr *qstr, char **name,
 				     void **value, size_t *len);
 int security_inode_create(struct inode *dir, struct dentry *dentry, umode_t mode);
-int security_inode_post_create(struct inode *dir, struct dentry *dentry,
-			       umode_t mode);
-
 int security_inode_link(struct dentry *old_dentry, struct inode *dir,
 			 struct dentry *new_dentry);
 int security_inode_unlink(struct inode *dir, struct dentry *dentry);
@@ -1829,9 +1821,6 @@ int security_file_send_sigiotask(struct task_struct *tsk,
 				 struct fown_struct *fown, int sig);
 int security_file_receive(struct file *file);
 int security_dentry_open(struct file *file, const struct cred *cred);
-int security_file_close(struct file *file);
-bool security_allow_merge_bio(struct bio *bio1, struct bio *bio2);
-
 int security_task_create(unsigned long clone_flags);
 void security_task_free(struct task_struct *task);
 int security_cred_alloc_blank(struct cred *cred, gfp_t gfp);
@@ -2160,13 +2149,6 @@ static inline int security_inode_create(struct inode *dir,
 	return 0;
 }
 
-static inline int security_inode_post_create(struct inode *dir,
-					     struct dentry *dentry,
-					     umode_t mode)
-{
-	return 0;
-}
-
 static inline int security_inode_link(struct dentry *old_dentry,
 				       struct inode *dir,
 				       struct dentry *new_dentry)
@@ -2367,16 +2349,6 @@ static inline int security_dentry_open(struct file *file,
 				       const struct cred *cred)
 {
 	return 0;
-}
-
-static inline int security_file_close(struct file *file)
-{
-	return 0;
-}
-
-static inline bool security_allow_merge_bio(struct bio *bio1, struct bio *bio2)
-{
-	return true; /* The default is to allow it for performance */
 }
 
 static inline int security_task_create(unsigned long clone_flags)
