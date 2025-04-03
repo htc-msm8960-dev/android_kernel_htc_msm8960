e0008ba9b8a070f941d381499f5e23be3d6e47bf
Yaroslav Zviezda <acroreiser@gmail.com>
2025-04-03 23:46:57 +0300

    HACK: cgroup: skip cgroup_rmdir

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4151,6 +4151,8 @@ static int cgroup_rmdir(struct inode *unused_dir, struct dentry *dentry)
 	struct cgroup_event *event, *tmp;
 	int ret;
 
+return 0;
+
 	/* the vfs holds both inode->i_mutex already */
 again:
 	mutex_lock(&cgroup_mutex);
